1c2978cb7f1e13caa587b94184e2f7b214ea73e0
Zakarya <zakaryamuratbaev13@gmail.com>
2022-05-03 14:01:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+"use strict";
+const arr = [1, 2, 3, 4, 5];
+// arr.unshift(656);
+// console.log(arr);
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
